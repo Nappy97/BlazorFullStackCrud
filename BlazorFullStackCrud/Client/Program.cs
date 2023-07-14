118129da4dff80c7263880b44637b72ba1f73d6f
nappy <85632877+Nappy97@users.noreply.github.com>
2023-07-14 17:07:50 +0900

    feat(All): CRUD with ef

---
+++ BlazorFullStackCrud/Client/Program.cs
@@ -0,0 +1,15 @@
+global using BlazorFullStackCrud.Client.Services.SuperHeroesService;
+global using BlazorFullStackCrud.Shared;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorFullStackCrud.Client;
+using BlazorFullStackCrud.Client.Services.SuperHeroesService;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<ISuperHeroService, SuperHeroService>();
+
+await builder.Build().RunAsync();
---
+++ BlazorFullStackCrud/Shared/SuperHero.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazorFullStackCrud.Shared;
+
+public class SuperHero
+{
+    public int Id { get; set; }
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string HeroName { get; set; } = string.Empty;
+
+    public Comic? Comic { get; set; }
+
+    public int ComicId { get; set; }
+}
